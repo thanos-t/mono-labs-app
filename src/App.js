c4833f7df52b1ed5b5e3f34852a672733b4e330a
thanasis <thanasisftw@gmail.com>
2020-12-24 22:52:07 +0200

    added font awesome icons to project, hooked shared styles and main app container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUsers, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+
+import './shared.scss';
+import MainContainer from './containers/MainContainer';
+
+library.add(faUsers, faQuestionCircle);
 
 const App = () => {
-  return <div>Mono Labs App</div>;
+  return (
+    <div>
+      <MainContainer />
+    </div>
+  );
 };
 
 export default App;
